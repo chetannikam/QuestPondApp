1abb0fd46c1006a36822cc0487ab6e5f24cb6af6
CHETAN\Admin <nikamchetan1998@gmail.com>
2022-10-19 20:21:51 +0530

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QuestPondApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "Home",//Key
+                url: "",///after domain or after slash
+                defaults: new { controller = "Home", action = "GoToHome", id = UrlParameter.Optional }//which controller and action to invoke for that URL
+            );
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuestPondApp.Models;
+namespace QuestPondApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Load()
+        {
+            Customer customerobj = new Customer
+            {
+                CustomerCode = "1001",
+                CustomerName = "Chetan Nikam"
+            };
+            return View("Customer",customerobj);
+        }
+        public ActionResult Enter()
+        {
+            return View("EnterCustomerNew",new Customer());/////new Customer() for the very first reuqest empty object is passed due to which empty value for input box
+        }
+        public ActionResult Submit(Customer datafromform)///////datafromform object will contain data if model property and form name parameter is same if not then use model binders 
+        {
+            //Customer obj = new Customer();
+            //obj.CustomerName = Request.Form["customerName"];////////////this steps happening intenally in datafromform obj
+            //obj.CustomerCode = Request.Form["customerId"];
+            if(ModelState.IsValid)
+            {
+                return View("Customer", datafromform);
+            }
+            else 
+            {
+                return View("EnterCustomerNew",datafromform);/////if not validated it will come here and will kept previous object value in text boxes
+            }
+            
+        }
+    }
+}
